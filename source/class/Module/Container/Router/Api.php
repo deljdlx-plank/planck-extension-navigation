9257fdd0c37a6f9042d6b705b78667fc8aa84093
Jean luc <you@example.com>
2019-02-13 12:47:30 +0100

    synchro

--- source/class/Module/Container/Router/Api.php
+++ source/class/Module/Container/Router/Api.php
@@ -25,6 +25,17 @@ class Api extends Router
 
         $this->get('get-routes', '`/navigation/get-routes`', function() {
 
+            $routes = $this->getApplication()->getRoutes();
+
+
+            echo '<pre id="' . __FILE__ . '-' . __LINE__ . '" style="border: solid 1px rgb(255,0,0); background-color:rgb(255,255,255)">';
+            echo '<div style="background-color:rgba(100,100,100,1); color: rgba(255,255,255,1)">' . __FILE__ . '@' . __LINE__ . '</div>';
+            print_r(array_keys($routes));
+            echo '</pre>';
+
+            die('EXIT '.__FILE__.'@'.__LINE__);
+
+
 
 
         })->json()
