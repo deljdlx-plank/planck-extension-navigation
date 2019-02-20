57195e936a023f177d63f9afca32c665f921eb4d
Jean luc <you@example.com>
2019-02-20 15:13:25 +0100

    synchro

--- install/sql/create.container.sql
+++ install/sql/create.container.sql
@@ -1,17 +1,11 @@
 CREATE TABLE navigation_container
 (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-
-
     status INT,
-
     qname VARCHAR(1024),
-
     name VARCHAR(1024),
-
     descriptor TEXT,
     properties TEXT,
-
     creation_date DATETIME,
     update_date DATETIME
 )
